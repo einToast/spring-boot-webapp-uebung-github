6c049a29df3ee02717ebf68cf95dc991374ee4cf
einToast <laurin2102@gmail.com>
2023-12-01 13:00:53 +0100

    - Testss

--- src/test/java/com/example/demo/DemoApplicationIT.java
+++ src/test/java/com/example/demo/DemoApplicationIT.java
@@ -3,11 +3,14 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class DemoApplicationIT {
 
 	@Test
 	void my_simple_integration_test() {
+		assertEquals(1, 1);
 		System.out.println("This is a integration test!");
 	}
 
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void my_simple_unit_test() {
+		assertEquals(1, 1);
 		System.out.println("This is a unit test!");
 	}
 
